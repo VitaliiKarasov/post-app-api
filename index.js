e6dbade198ef4edfa02d4764e04c178f99178374
Vitalii Karasov <vitaliikarasov@Vitaliis-MacBook-Air.local>
2022-12-07 11:09:36 +0200

    add proxy

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://postapp.onrender.com');
     next();
   });
 
